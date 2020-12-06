43209636ab86727f61c2ec814ec7cff663186e44
DianaKuznetsova <dianakyznetsova@yandex.by>
2020-12-06 14:34:18 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ISP_Lab13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
